268e0b27b491fcdfad84b1120e471db06c7c3ba0
Espéré Jonathan <hei.espere@gmail.com>
2023-11-19 21:35:02 +0300

    Feat: postgres connection

---
+++ src/main/java/model/PostgresqlConnection.java
@@ -0,0 +1,43 @@
+package model;
+
+import lombok.Getter;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+@Getter
+public class PostgresqlConnection{
+    private Connection connection;
+    private Statement statement;
+    private static PostgresqlConnection oneInstance;
+    private PostgresqlConnection(){
+        try {
+            this.connection = DriverManager.getConnection(
+                    Env.DB_URL,
+                    Env.DB_USERNAME,
+                    Env.DB_PASSWORD
+            );
+            this.statement = this.connection.createStatement();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static PostgresqlConnection getNewInstance() {
+        if(PostgresqlConnection.oneInstance == null){
+            PostgresqlConnection.oneInstance = new PostgresqlConnection();
+        }
+        return PostgresqlConnection.oneInstance;
+    }
+
+    public void closeConnection(){
+        try {
+            this.connection.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
